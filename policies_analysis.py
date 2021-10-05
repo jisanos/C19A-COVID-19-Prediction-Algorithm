c807aff6f84247855af1d68cf1f34d3a6693811a
jisanos <44531911+jisanos@users.noreply.github.com>
2021-10-05 19:19:47 -0400

    Merged us vaccine dataset with global dataset

--- policies_analysis.py
+++ policies_analysis.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 import re
 import nltk
 
-#nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.snowball import SnowballStemmer
-#nltk.download('wordnet')
+# nltk.download('wordnet')
 from nltk.stem.wordnet import WordNetLemmatizer
 import seaborn as sns
 from wordcloud import WordCloud
@@ -25,14 +25,15 @@ from wordcloud import WordCloud
 policies_path = ".\\CCI_C-19_Policies\\data_tables\\policy_data\\table_data\\Current\\"
 policies_files = glob.glob(policies_path + "*.csv")
 
-content = [] #store contents from files
+content = []  # store contents from files
 
 for filepath in policies_files:
-    
+
     df = pd.read_csv(filepath, index_col=None)
-    #Stripping the string to store the name of the file to a State column
-    #State column should be the second column
-    df.insert(1,"State", filepath.replace(policies_path,"").replace("_policy.csv",""))
+    # Stripping the string to store the name of the file to a State column
+    # State column should be the second column
+    df.insert(1, "State", filepath.replace(
+        policies_path, "").replace("_policy.csv", ""))
     content.append(df)
 
 
@@ -41,7 +42,7 @@ policies_df = pd.concat(content)
 # %%
 # Getting all unique values from the columns after "policy" columns in the dataframe
 for col in policies_df.columns[3:]:
-    print(col,": ",set(policies_df[col]))
+    print(col, ": ", set(policies_df[col]))
 
 
 # %%
@@ -49,39 +50,41 @@ for col in policies_df.columns[3:]:
 # previous cell.
 
 # Replacing only single lowercase y to Y
-policies_df.replace(to_replace=r'^y$',value="Y",regex=True,inplace=True)
+policies_df.replace(to_replace=r'^y$', value="Y", regex=True, inplace=True)
 
 
 # %%
 # Finding which entry has a whitespace
-print(policies_df[policies_df['Restrict/Close'].str.contains("\s", regex=True, na=False)])
+print(
+    policies_df[policies_df['Restrict/Close'].str.contains("\s", regex=True, na=False)])
 
 
 # %%
 # Reading the policy from entry
-list(policies_df["policy"][policies_df['Restrict/Close'].str.contains("\s", regex=True, na=False)])
+list(policies_df["policy"]
+     [policies_df['Restrict/Close'].str.contains("\s", regex=True, na=False)])
 
 # %% [markdown]
 # Policy doesnt provide enough info so will just fill with NaN in the meantime.
 
 # %%
-policies_df['Restrict/Close'].replace("\s", np.nan ,inplace=True,regex=True)
+policies_df['Restrict/Close'].replace("\s", np.nan, inplace=True, regex=True)
 
 
 # %%
 # Re-verifying all unique values
 for col in policies_df.columns[3:]:
-    print(col,": ",set(policies_df[col]))
+    print(col, ": ", set(policies_df[col]))
 
 
 # %%
 # Dropping, if any, duplicates that are contained from the first 3 cols
-policies_df.drop_duplicates(["date","State","policy"], inplace=True)
+policies_df.drop_duplicates(["date", "State", "policy"], inplace=True)
 
 
 # %%
 # Checking dups of only first 2 cols
-policies_df[policies_df.duplicated(["date","State"], keep= False)]
+policies_df[policies_df.duplicated(["date", "State"], keep=False)]
 
 # %% [markdown]
 # There seem to be multiple entries per date,state. Maybe merging them could be a good idea.
@@ -108,11 +111,11 @@ for col in cols:
     # Property of joining with commas for the agg
     dic[col] = ",".join
 
-policies_df = policies_df.groupby(["date","State"]).agg(dic).reset_index()
+policies_df = policies_df.groupby(["date", "State"]).agg(dic).reset_index()
 
 
 # %%
-#policies_df
+# policies_df
 
 
 # %%
@@ -122,32 +125,36 @@ policies_df = policies_df.groupby(["date","State"]).agg(dic).reset_index()
 
 def reducto(df):
     dic = {}
-    
-    cols = list(df.columns)[3:] #Only the columns 4th and forward
-    
+
+    cols = list(df.columns)[3:]  # Only the columns 4th and forward
+
     for col in cols:
         for val in df[col]:
-            val = val.strip()#Remove any whitespaces
-            split = val.split(',')#Split on comma
+            val = val.strip()  # Remove any whitespaces
+            split = val.split(',')  # Split on comma
             if len(split) > 1:
                 if "Y" in split:
                     dic[col] = "Y"
                 else:
                     dic[col] = np.nan
-    return pd.Series(dic, index=cols, dtype = object)
+    return pd.Series(dic, index=cols, dtype=object)
 
-policies_df = policies_df.groupby(["date","State","policy"]).apply(reducto).reset_index()  
+
+policies_df = policies_df.groupby(
+    ["date", "State", "policy"]).apply(reducto).reset_index()
 
 
 # %%
 # Counting most common words, inspired by
 # https://www.houseninetytwo.com/how-to-use-python-to-extract-keywords-from-sentance-in-dataframe/
-common_words = pd.Series(' '.join(policies_df['policy']).split()).value_counts()[:40]
+common_words = pd.Series(
+    ' '.join(policies_df['policy']).split()).value_counts()[:40]
 # common_words
 
 
 # %%
-uncommon_words = pd.Series(' '.join(policies_df['policy']).split()).value_counts()[-40:]
+uncommon_words = pd.Series(
+    ' '.join(policies_df['policy']).split()).value_counts()[-40:]
 # uncommon_words
 
 
@@ -161,16 +168,17 @@ policies_df['policy'] = policies_df['policy'].str.lower()
 #stemmer = SnowballStemmer("english")
 lemma = nltk.wordnet.WordNetLemmatizer()
 
+
 def lemmatizing(x):
-    
+
     words = str(x).split(" ")
     lemd = []
-    
+
     for word in words:
         lemd.append(lemma.lemmatize(word))
-    
+
     return " ".join(lemd)
-    
+
 
 policies_df['policy'] = policies_df['policy'].apply(lemmatizing)
 
@@ -179,35 +187,41 @@ policies_df['policy'] = policies_df['policy'].apply(lemmatizing)
 # Remove duplicate words within the same row, or cell.
 tokenizer = nltk.WordPunctTokenizer()
 
+
 def rem_dup(x):
-    x = x.strip(",.;") #replace(",","").replace(".","").replace(";","")#removing commas,periods,semicolons
-     
-    #words = x.split(' ') #splitting words by space
-    words = tokenizer.tokenize(x)# Splitting words by tokenization
-    
-    #Stripping words of extra whitespaces
+    # replace(",","").replace(".","").replace(";","")#removing commas,periods,semicolons
+    x = x.strip(",.;")
+
+    # words = x.split(' ') #splitting words by space
+    words = tokenizer.tokenize(x)  # Splitting words by tokenization
+
+    # Stripping words of extra whitespaces
     stripped_words = [word.strip() for word in words]
-    
-    unique_words = set(stripped_words) #This will only keep unique words/remove duplicate words
-        
-    unique_words_str = " ".join(unique_words) #Turning the list back to a string
-    
+
+    # This will only keep unique words/remove duplicate words
+    unique_words = set(stripped_words)
+
+    # Turning the list back to a string
+    unique_words_str = " ".join(unique_words)
+
     return unique_words_str
-    
+
+
 policies_df['policy'] = policies_df['policy'].apply(rem_dup)
 
 
 # %%
-#list of stop words (i can add more words if necesary)
+# list of stop words (i can add more words if necesary)
 stop_words = set(stopwords.words('english'))
 # stop_words = stop_words.union([""]) #To add custom/extra words
-#stop_words
+# stop_words
 
 
 # %%
 # Remove useless words using the stop words
-policies_df['policy'] = policies_df['policy'].apply(lambda x: ' '.join(item for item in x.split() if item not in stop_words))
-#policies_df.head(50)
+policies_df['policy'] = policies_df['policy'].apply(
+    lambda x: ' '.join(item for item in x.split() if item not in stop_words))
+# policies_df.head(50)
 
 
 # %%
@@ -215,15 +229,16 @@ policies_df['policy'] = policies_df['policy'].apply(lambda x: ' '.join(item for 
 def pointless(x):
     words = x.split(" ")
     keep = []
-    
+
     for word in words:
-        #if word has a lenght between...
+        # if word has a lenght between...
         if len(word) < 3 and len(word) >= 1:
-            continue #Ignore these words
+            continue  # Ignore these words
         else:
             keep.append(word)
     return ' '.join(keep)
-    
+
+
 policies_df['policy'] = policies_df['policy'].apply(pointless)
 
 
@@ -233,11 +248,11 @@ policies_df['policy'] = policies_df['policy'].apply(pointless)
 # def remove_special(x):
 #     words = str(x).split(" ")
 #     stripped = []
-    
+
 #     for word in words:
 # #         print(word)
 # #         print(re.sub(r'[^A-Za-z]+','',word))
-        
+
 #         if word != (re.sub(r'[^A-Za-z]+','',word)):
 #             print(word)
 #             print(re.sub(r'[^A-Za-z]+','',word))
@@ -250,7 +265,8 @@ policies_df['policy'] = policies_df['policy'].apply(pointless)
 
 # %%
 # Adding a word count column
-policies_df.insert(3, "word_count", policies_df['policy'].apply(lambda x: len(str(x).split(" "))))
+policies_df.insert(3, "word_count", policies_df['policy'].apply(
+    lambda x: len(str(x).split(" "))))
 policies_df
 
 
@@ -262,38 +278,40 @@ policies_df.word_count.describe()
 # %%
 # Number of words and unique words
 total_words = pd.Series(' '.join(policies_df['policy']).split())
-print("Total wods: ",len(total_words))
-print("Unique Words: ",len(set(total_words)))
+print("Total wods: ", len(total_words))
+print("Unique Words: ", len(set(total_words)))
 
 
 # %%
 # Creating a violinplot from wordcount
-sns.violinplot(y = 'word_count',data = policies_df)
+sns.violinplot(y='word_count', data=policies_df)
 
 
 # %%
-#refreshing most common words now that we removed stop words
-word_frequency = pd.Series(' '.join(policies_df['policy']).split()).value_counts()
-print("Common Words: ",word_frequency.head(15))
-print("Uncommon Words: ",word_frequency.tail(15))
+# refreshing most common words now that we removed stop words
+word_frequency = pd.Series(
+    ' '.join(policies_df['policy']).split()).value_counts()
+print("Common Words: ", word_frequency.head(15))
+print("Uncommon Words: ", word_frequency.tail(15))
 
 
 # %%
-plt.figure(figsize=(15,4))
+plt.figure(figsize=(15, 4))
 plt.xticks(rotation=90)
 plt.bar(word_frequency.index[:100], word_frequency[:100])
 
 
 # %%
-plt.figure(figsize=(15,4))
+plt.figure(figsize=(15, 4))
 plt.xticks(rotation=50)
 plt.bar(word_frequency.index[-40:], word_frequency[-40:])
 
 
 # %%
-#Creating a wordcloud
-wordcloud = WordCloud(width=1600,height=1600,collocations=False,background_color='black').generate(" ".join(total_words))
-plt.figure(figsize=(20,20))
+# Creating a wordcloud
+wordcloud = WordCloud(width=1600, height=1600, collocations=False,
+                      background_color='black').generate(" ".join(total_words))
+plt.figure(figsize=(20, 20))
 plt.imshow(wordcloud)
 
 
@@ -302,41 +320,41 @@ plt.imshow(wordcloud)
 
 # %% [markdown]
 # #### Policies Dictionary
-# 
+#
 # No column dictionary was provided in the repository for these. Will make my own.
-# 
+#
 # **The following are the primary cols**
-# 
+#
 # date: date it occured
-# 
+#
 # State: string of State
-# 
+#
 # policy: string of actions taken by the state
-# 
+#
 # **The rest are just booleans, of which are mostly either Y or nan**
-# 
+#
 # Restrict/Close: Closure of unesential businesses?
-# 
+#
 # Opening (State): Opened the state to foreigners?
-# 
+#
 # Deferring to County: ?
-# 
+#
 # Testing: Covid testing began?
-# 
+#
 # Education: Physical attendance of education again?
-# 
+#
 # Health/Medical: ?
-# 
+#
 # Emergency Level: ?
-# 
+#
 # Transportation: Public transportation??
-#        
+#
 # Budget: ?
-#        
+#
 # Social Distancing: Social distancing was active?
-# 
+#
 # Other: ?
-#        
+#
 # Vaccine: Vaccines were active?
-# 
+#
 # Opening (County): ?
--- vaccines_analysis.py
+++ vaccines_analysis.py
@@ -30,7 +30,7 @@ ts_vaccine_df = pd.read_csv(
 # ts_people_vaccine_us_df = pd.read_csv(
 #     ".\\CCI_C-19\\data_tables\\vaccine_data\\us_data\\time_series\\people_vaccinated_us_timeline.csv")
 
-#This contains vaccine type per state
+# This contains vaccine type per state
 ts_vaccine_us_df = pd.read_csv(
     ".\\CCI_C-19\\data_tables\\vaccine_data\\us_data\\time_series\\vaccine_data_us_timeline.csv")
 
@@ -39,56 +39,54 @@ ts_vaccine_us_df = pd.read_csv(
 # Metric Name 	                Definition
 # Country_Region 	            Country or region name
 # Date 	                        Data collection date
-# Doses_admin 	                Cumulative number of doses administered. When 
-#                               a vaccine requires multiple doses, each one 
-                                #is counted independently
-# People_partially_vaccinated 	Cumulative number of people who received at 
-                                #least one vaccine dose. When the person 
-                                #receives a prescribed second dose, it is not 
-                                #counted twice
-# People_fully_vaccinated 	    Cumulative number of people who received all 
-                                #prescribed doses necessary to be considered 
-                                #fully vaccinated
+# Doses_admin 	                Cumulative number of doses administered. When
+#                               a vaccine requires multiple doses, each one
+# is counted independently
+# People_partially_vaccinated 	Cumulative number of people who received at
+# least one vaccine dose. When the person
+# receives a prescribed second dose, it is not
+# counted twice
+# People_fully_vaccinated 	    Cumulative number of people who received all
+# prescribed doses necessary to be considered
+# fully vaccinated
 # Report_Date_String 	        Data reported date
 # UID 	                        Country code: https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv
 
 
 # %%
-#print(vaccine_df.columns)
+# print(vaccine_df.columns)
 
 # %%
 
-#This only contains current global data, and is not useful to
-#train our algorithm
+# This only contains current global data, and is not useful to
+# train our algorithm
 
-#print(vaccine_df[vaccine_df.notna()])
+# print(vaccine_df[vaccine_df.notna()])
 
 # %%
-
-print(ts_vaccine_df.dtypes)
-
 # Changing date column to a date variable
 
 ts_vaccine_df['Date'] = ts_vaccine_df['Date'].astype('datetime64[ns]')
-
-print(ts_vaccine_df.dtypes)
-# %%
-
-# Creating a dataframe of only 
+ts_vaccine_us_df['Date'] = ts_vaccine_us_df['Date'].astype('datetime64[ns]')
 
 # %%
 
-ts_vaccine_df[ts_vaccine_df['Province_State'] == "NewYork"].plot('Date','People_partially_vaccinated')
-plt.show()
+# Creating a dataframe of only
+
+# %%
+
+# ts_vaccine_df[ts_vaccine_df['Province_State'] == "NewYork"].plot('Date','People_partially_vaccinated')
+# plt.show()
 
 # %%
 unique_global_regions = set(ts_vaccine_df["Country_Region"])
-print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "US"]["Province_State"]))
-print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "US (Aggregate)"]["Province_State"]))
-print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "World"]["Province_State"]))
+# print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "US"]["Province_State"]))
+# print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "US (Aggregate)"]["Province_State"]))
+# print(set(ts_vaccine_df[ts_vaccine_df['Country_Region'] == "World"]["Province_State"]))
 unique_global_states = set(ts_vaccine_df['Province_State'])
 
-unique_global_regions_with_nan_states = set(ts_vaccine_df[~ts_vaccine_df['Province_State'].notna() ]['Country_Region'])
+unique_global_regions_with_nan_states = set(
+    ts_vaccine_df[~ts_vaccine_df['Province_State'].notna()]['Country_Region'])
 
 # %%
 
@@ -98,29 +96,65 @@ unique_global_regions_with_nan_states = set(ts_vaccine_df[~ts_vaccine_df['Provin
 # This means i gotta import and merge the us data with the global data
 
 
-
-
 # %%
 # Dropping all of the US values from the global dataframe
 # as well as World values
+to_remove = ["US", "US (Aggregate)", "World"]
 
-ts_vaccine_df = ts_vaccine_df[~(ts_vaccine_df["Country_Region"] == "US")]
-ts_vaccine_df = ts_vaccine_df[~(ts_vaccine_df["Country_Region"] == "US (Aggregate)")]
-ts_vaccine_df = ts_vaccine_df[~(ts_vaccine_df["Country_Region"] == "World")]
+for element in to_remove:
+
+    ts_vaccine_df = ts_vaccine_df[~(
+        ts_vaccine_df["Country_Region"] == element)]
+
 
 # %%
 
-unique_us_states= set(ts_vaccine_us_df['Province_State'])
+unique_us_states = set(ts_vaccine_us_df['Province_State'])
 
 # %%
-# Creating a list of "states" in the ts_vaccine_us_df which arent really states:
-    #Long Term Care (LTC) Program
-    #Veterans Health Administration
-    #Bureau of Prisons
-    #Department of Defense
-not_really_states = {"Long Term Care (LTC) Program","Veterans Health Administration",
-                     "Bureau of Prisons","Department of Defense"}
+# Creating a list of "states" in the ts_vaccine_us_df which arent really states
+# from the US dataframe:
+not_really_states = ["Long Term Care (LTC) Program",
+                     "Veterans Health Administration",
+                     "Bureau of Prisons", "Department of Defense",
+                     "Federal Bureau of Prisons", "Indian Health Services"]
+
+# Dropping them
+for element in not_really_states:
+    ts_vaccine_us_df = ts_vaccine_us_df[~(
+        ts_vaccine_us_df['Province_State'] == element)]
+
 
 # %%
+# Checking if states in us dataset are in global dataset
+
+unique_us_states = set(ts_vaccine_us_df['Province_State'])
+unique_global_states = set(ts_vaccine_df['Province_State'])
+
+in_both = [
+    element for element in unique_us_states if element in unique_global_states]
+
+# According to this, they both have unique values of state by this point
+# %%
+# Adding Vaccine_Type column equaling to all for the global dataset for the merging
+ts_vaccine_df["Vaccine_Type"] = "All"
+
+# %%
+# The column "People_partially_vaccinated" from the global dataset is the most
+# similar to "Stage_One_Doses" from the US dataset
+# This goes the same for the People_fully_vaccinated and Stage_two_doses
+# so these will be renamed for the merger
+
+ts_vaccine_df.rename(columns={"People_partially_vaccinated": "Stage_One_Doses",
+                              "People_fully_vaccinated": "Stage_Two_Doses"}, inplace=True)
+
+# These columns seem to contain lots of nans at times so it might not be as
+# important as Doses_admin
+
+# %%
+merge_df = pd.merge(ts_vaccine_us_df, ts_vaccine_df, how='outer',
+                    on=['Country_Region', 'Province_State', 'Vaccine_Type', 'Doses_admin',
+                        'Stage_One_Doses', 'Stage_Two_Doses', 'Date'])
 
 
+# Merging global dataframe with us dataframe
