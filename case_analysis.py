d2d64c732e815c643f2b2d604f1c58f42a1200dd
jisanos <44531911+jisanos@users.noreply.github.com>
2021-10-11 13:09:35 -0400

    Further cleaning of data

--- case_analysis.py
+++ case_analysis.py
@@ -18,9 +18,16 @@ import numpy as np
 # %%
 
 csse_covid_19_daily_reports = data_imports.csse_covid_19_daily_reports()
+
 # Droppin all NaN rows
-csse_covid_19_daily_reports.dropna(how='all',inplace = True)
-csse_covid_19_daily_reports.reset_index(inplace = True)
+csse_covid_19_daily_reports.dropna(how='all', inplace=True)
+csse_covid_19_daily_reports.reset_index(inplace=True)
+
+# Setting datatypes
+csse_covid_19_daily_reports['date'] = csse_covid_19_daily_reports['date'].astype(
+    'datetime64[ns]')
+
+csse_covid_19_daily_reports = csse_covid_19_daily_reports.convert_dtypes()
 
 # %%
 
@@ -85,11 +92,11 @@ csse_covid_19_daily_reports['Province_State'] = csse_covid_19_daily_reports[
 # index_state_B = csse_covid_19_daily_reports[
 #     csse_covid_19_daily_reports['Province/State'].notna()].index
 
-# intersection_2 = index_state_A.intersection(index_state_B) 
+# intersection_2 = index_state_A.intersection(index_state_B)
 
 # %% Dropping cols
-csse_covid_19_daily_reports= csse_covid_19_daily_reports.drop(
-    ['Province/State','Country/Region'],axis=1)
+# csse_covid_19_daily_reports= csse_covid_19_daily_reports.drop(
+#     ['Province/State','Country/Region'],axis=1)
 
 # %% Checking how many rows dont contain lat and long
 
@@ -113,7 +120,7 @@ csse_covid_19_daily_reports['Lat'] = csse_covid_19_daily_reports[
 csse_covid_19_daily_reports['Long_'] = csse_covid_19_daily_reports[
     'Long_'].fillna(csse_covid_19_daily_reports['Longitude'])
 
-# %% re-verifying 
+# %% re-verifying
 
 # rows_with_lat = csse_covid_19_daily_reports[
 #      csse_covid_19_daily_reports['Lat'].notna()].index
@@ -129,9 +136,8 @@ csse_covid_19_daily_reports['Long_'] = csse_covid_19_daily_reports[
 
 
 # %% Dropping cols
-csse_covid_19_daily_reports = csse_covid_19_daily_reports.drop(
-    ['Latitude','Longitude'],axis = 1)
-
+# csse_covid_19_daily_reports = csse_covid_19_daily_reports.drop(
+#     ['Latitude','Longitude'],axis = 1)
 
 
 # %% List of counties with no Lat or Long
@@ -152,19 +158,21 @@ unique_province_states = set(csse_covid_19_daily_reports['Province_State'])
 
 unique_country_regions = set(csse_covid_19_daily_reports['Country_Region'])
 
-# %% List of countries that need to be merged
-merge_countries = [('Bahamas','Bahamas, The'),
-                   ('Czech Republic','Czechia'),
-                   ('Gambia','Gambia, The'),
-                   ('Hong Kong','Hong Kong SAR'),
-                   ('Iran','Iran (Islamic Republic of)'),
-                   ('Macau','Macao SAR'),
-                   ('Russia','Russian Federation'),
-                   ('South Korea',''),
-                   ('Taiwan','Taiwan*')]
+# %% List of countries that might need to be merged
+merge_countries = [('Bahamas', 'Bahamas, The'),
+                   ('Czech Republic', 'Czechia'),
+                   ('Gambia', 'Gambia, The'),
+                   ('Hong Kong', 'Hong Kong SAR'),
+                   ('Iran', 'Iran (Islamic Republic of)'),
+                   ('Macau', 'Macao SAR'),
+                   ('Russia', 'Russian Federation'),
+                   ('South Korea', 'Korea, South'),
+                   ('Taiwan', 'Taiwan*'),
+                   ('Mainland China', 'China'),  # Maybe?
+                   ]
 
 # Noticed hat Puerto Rico is set as country as well as State within us country
-# must check if there are any 
+# must check if there are any
 # %%
 
 # PR_as_country = csse_covid_19_daily_reports[csse_covid_19_daily_reports[
@@ -190,22 +198,117 @@ csse_covid_19_daily_reports = csse_covid_19_daily_reports.drop(
 # PR_as_country = csse_covid_19_daily_reports[csse_covid_19_daily_reports[
 #     'Country_Region'] == 'Puerto Rico']
 
+# %%
+# Checking differences between Case_Datality_Ratio and Case-Fatality_Ratio
+# cols.
+# csse_covid_19_daily_reports[csse_covid_19_daily_reports['Case-Fatality_Ratio'].notna()]
+# csse_covid_19_daily_reports[csse_covid_19_daily_reports['Case_Fatality_Ratio'].notna()]
+
+# %% Filling Case_Fatality_Ratio nans with Case-Fatality_Ratio
+
+csse_covid_19_daily_reports['Case_Fatality_Ratio'
+                            ] = csse_covid_19_daily_reports[
+                                'Case_Fatality_Ratio'].fillna(
+                                    csse_covid_19_daily_reports[
+                                        'Case-Fatality_Ratio'])
+
+# %% Dropping 'Case-Fatality_Ratio' columns
+
+# csse_covid_19_daily_reports = csse_covid_19_daily_reports.drop(
+#     'Case-Fatality_Ratio',axis = 1)
+
+# %%
+# Comparing insidence rates columns
+
+
+# csse_covid_19_daily_reports[csse_covid_19_daily_reports['Incident_Rate'].notna()]
+# csse_covid_19_daily_reports[csse_covid_19_daily_reports['Incidence_Rate'].notna()]
+
+# %% Filling Incident_Rate nans with Incidence_Rate nans
+csse_covid_19_daily_reports['Incident_Rate'
+                            ] = csse_covid_19_daily_reports[
+                                'Incident_Rate'].fillna(
+                                    csse_covid_19_daily_reports[
+                                        'Incidence_Rate'])
+
+# %% Dropping Incidence_Rate column
+
+# csse_covid_19_daily_reports = csse_covid_19_daily_reports.drop(
+#     'Incidence_Rate',axis = 1)
+
+# %% Dropping unnecesary columns
+
+unnecessary_cols = ['Last_Update', 'Combined_Key',
+                    'Last Update', 'Incidence_Rate', 'Case-Fatality_Ratio',
+                    'Latitude', 'Longitude', 'Province/State', 'Country/Region',
+                    'index'
+                    ]
+
+csse_covid_19_daily_reports.drop(unnecessary_cols, axis='columns',
+                                 inplace=True)
+
+
+# %% removing whitespaces from string columns
+
+cols_to_strip = ['Admin2', 'Province_State', 'Country_Region']
+
+for col in cols_to_strip:
+    csse_covid_19_daily_reports[col] = csse_covid_19_daily_reports[
+        col].str.strip()
+
+# %% There are some columns which contain nan in all important values
+# should take care of them somehow
+
+# %% Case_Fatality_Ratio contains some "stringed" numbers which does not let it
+# become a float dtype
+#
+# There are some #DIV/0! in the cells which doesnt allow it to become float
+# Removing these should work
+
+csse_covid_19_daily_reports['Case_Fatality_Ratio'
+                            ] = csse_covid_19_daily_reports[
+                                'Case_Fatality_Ratio'].str.replace('#DIV/0!', '')
+
+# %% Turning to float type
+
+
+csse_covid_19_daily_reports['Case_Fatality_Ratio'
+                            ] = csse_covid_19_daily_reports[
+                                'Case_Fatality_Ratio'].astype("Float64")
+
+# %% Fixing negative values
+
+cols_to_fix = ['Confirmed', 'Deaths', 'Recovered', 'Active', 'Incident_Rate',
+               'Case_Fatality_Ratio']
+
+for col in cols_to_fix:
+    csse_covid_19_daily_reports[col] = csse_covid_19_daily_reports[col].abs()
+
 # %% List of not real country regions within the dataset
 
 non_country_regions = ['Summer Olympics 2020']
 
+# %% List of unique province_state
+
+unique_province_states = set(csse_covid_19_daily_reports['Province_State'])
+
+# %% List of unique Country_Region
+
+unique_country_regions = set(csse_covid_19_daily_reports['Country_Region'])
 
 # %%
 
-# Dropping unnecesary columns:
-# Last update is not necesary as Date is more important
-# Combined_Key is redundant
-# Latitude and longitude are reduntand as well as contain more NANs than Lat
-# Long_
-#
-    
-unnecessary_cols=['Last_Update','Combined_Key',
-                  'Last Update']
+unique_counties = set(csse_covid_19_daily_reports['Admin2'])
 
-csse_covid_19_daily_reports.drop(unnecessary_cols,axis = 'columns',
-                                  inplace= True)
+# %% List of countries that might need to be merged
+merge_countries = [('Bahamas', 'Bahamas, The'),
+                   ('Czech Republic', 'Czechia'),
+                   ('Gambia', 'Gambia, The'),
+                   ('Hong Kong', 'Hong Kong SAR'),
+                   ('Iran', 'Iran (Islamic Republic of)'),
+                   ('Macau', 'Macao SAR'),
+                   ('Russia', 'Russian Federation'),
+                   ('South Korea', 'Korea, South'),
+                   ('Taiwan', 'Taiwan*'),
+                   ('Mainland China', 'China'),  # Maybe?
+                   ]
