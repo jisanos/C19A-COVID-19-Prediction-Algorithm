db9e662c1ef0a5ccda4ac5afe1b07825dbdd184d
Seals <59105007+RaynielRamirezz@users.noreply.github.com>
2021-10-15 20:46:15 -0400

    Made the map a time series and some adjustments

--- cases_cleaned_visual_analysis.py
+++ cases_cleaned_visual_analysis.py
@@ -12,11 +12,15 @@ import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
 import folium
+from folium import plugins
+from folium.plugins import HeatMap
 from numpy import nan
 from numpy import isnan
 from pandas import read_csv
 from sklearn.impute import SimpleImputer
-import webbrwoser
+import webbrowser
+import random
+
 #set the plot's theme to something more beautiful
 sns.set()
 # %%
@@ -29,31 +33,33 @@ cases_cleaned = cases_cleaned[cases_cleaned["date"] == "2021-09-28"]
 
 # %%
 #Take away the unnessary columns and only use longitude, latitude and the countries for the map specifically
-cases_cleaned_locations = cases_cleaned[["Country_Region","Lat", "Long_"]]
+cases_cleaned_locations = cases_cleaned[["Deaths","Lat", "Long_"]]
 
 # %%
 #removing nan values and replacing them with 0 for the time being
 cases_cleaned_locations_NA_removed = cases_cleaned_locations.fillna(8)
-#print(cases_cleaned_locations.isnull().sum())
-#cases_cleaned_locations_NA_removed = cases_cleaned_locations.dropna(inplace=True)
-# mark zero values as missing or NaN
-#cases_cleaned_locations_NA_removed[["Lat", "Long_"]] = dataset[[""]].replace(nan, 1.2)
 
 # %%
-# Centering the map with the latitude and longitude mean values
+# Centering the map with the latitude and longitude mean values 
+
 covid_country_map = folium.Map(location=[cases_cleaned_locations_NA_removed["Lat"].mean(), 
-                                         cases_cleaned_locations_NA_removed["Long_"].mean()], 
-                                        zoom_start=14, control_scale=True)
+                                        cases_cleaned_locations_NA_removed["Long_"].mean()], 
+                                       zoom_start=4, control_scale=True)
 
+#heat_info = [[info["Lat"],info["Long_"],info["Deaths"]] for index, 
+ #            info in cases_cleaned_locations_NA_removed.iterrows()]
+
+heat_info = [[[info["Lat"],info["Long_"]] for index, 
+             info in cases_cleaned_locations_NA_removed[cases_cleaned_locations_NA_removed['Deaths'] == i].iterrows()] for i in range(0,1000)]
+
+hm = plugins.HeatMapWithTime(heat_info,auto_play=True, min_opacity=0.3, radius=29)
+
+#hm = plugins.HeatMapWithTime(heat_info,auto_play=True, min_opacity=0.3, 
+ #            radius=29,blur=25,max_zoom=1)
+
+hm.add_to(covid_country_map)
+
+#covid_country_map.add_child(hm)
 # %%
-for index, info in cases_cleaned_locations_NA_removed.iterrows():
-    folium.Marker([info["Lat"], info["Long_"]], 
-                  popup=info["Country_Region"]).add_to(covid_country_map)
-# %%    
+#HeatMap(heat_data).add_to(map_hooray)  
 covid_country_map.save("mymap.html")   
-
-# %%
-m = folium.Map(location=[45.5236, -122.6750])
-m.save("mymap.html") 
-# %%
-display(m)
