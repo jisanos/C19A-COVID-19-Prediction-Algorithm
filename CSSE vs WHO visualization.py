c07e3099fcc57f0c5318b07ab4f29fea86ebd9d5
LuisRodriguez01 <36483056+LuisRodriguez01@users.noreply.github.com>
2021-10-19 14:59:42 -0400

    Managed to do Top 10 now trying Other Countries

--- CSSE vs WHO visualization.py
+++ CSSE vs WHO visualization.py
@@ -27,31 +27,52 @@ Last_day = df.loc[(df['date'] == '2021-09-21')]
 
 print(Last_day)
 
+#%%
+#Top 10 results for death cases in countries
+
+Top_10 = Last_day.nlargest(10,'Deaths')
+
+print(Top_10)
+
+#Other countries
+
+Other_Countries = Last_day.nsmallest(3978,'Deaths')
+
+Other_Countries.head()
 #%% 
 #plotting bubble plot based on data
 
 #reason the y axis has exponential value on top is because the number is too big so 7 is actually 7M
 
-sns.scatterplot(data=Last_day, x="Deaths", y="Confirmed", size=200, legend=False, sizes=(20, 2000))
+sns.scatterplot(data=Top_10, x="Deaths", y="Confirmed", size=200, legend=False, sizes=(20, 2000))
 
-x,y= Last_day['Deaths'],Last_day['Confirmed']
+x,y= Top_10['Deaths'],Top_10['Confirmed']
 
-for i, txt in enumerate(Last_day.Country_Region):
-    plt.annotate(txt,(Last_day.Deaths.iat[i],Last_day.Confirmed.iat[i]))
+for i, txt in enumerate(Top_10.Country_Region):
+    plt.annotate(txt,(Top_10.Deaths.iat[i],Top_10.Confirmed.iat[i]))
 
+#Trying to add Other countries label as one
+for i, txt in enumerate(Other_Countries.Country_Region):
+    plt.annotate(txt,(Other_Countries.Deaths.iat[i],Other_Countries.Confirmed.iat[i]))
 #%%
 
 
 #varaibles for plot
 
 
-plt.scatter(x = Last_day['Deaths'],
-            y = Last_day['Confirmed'],
-            s = Last_day['Recovered']/1000,
+plt.scatter(x = Top_10['Deaths'],
+            y = Top_10['Confirmed'],
+            s = Top_10['Recovered']/1000,
             alpha = 0.5)
 
+#Trying to add Other regions to same bubble chart
+plt.scatter(x = Other_Countries['Deaths'],
+           y = Other_Countries['Confirmed'],
+           s= Other_Countries['Recovered']/1000,
+           alpha = 0.5)
+
 #Title and labels
-plt.title('Country/Regions Bubble Chart')
+plt.title('Top 10 Country/Regions(Deaths) Bubble Chart')
 plt.xlabel('Deaths')
 plt.ylabel('Confirmed')
 
