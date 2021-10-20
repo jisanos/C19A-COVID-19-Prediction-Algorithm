9f5f2902dd24846993a208a5024ed4d1b42315ad
Seals <59105007+RaynielRamirezz@users.noreply.github.com>
2021-10-19 23:40:05 -0400

    created bar graphs and a heatmap where it shows the confirmed values by year and month by preprocessing data for its convenience

--- cases_cleaned_visual_analysis.py
+++ cases_cleaned_visual_analysis.py
@@ -23,6 +23,7 @@ import random
 import datetime
 import time
 from datetime import datetime
+import calendar
 
 #set the plot's theme to something more beautiful
 sns.set()
@@ -30,12 +31,9 @@ sns.set()
 ## open cases_cleaned.csv and store it in an dataframe and then filter the date to the newest date
 file = ".\\cases_cleaned.csv"
 cases_cleaned = pd.read_csv(file)
-cases_cleaned_Copy = pd.read_csv(file)
-
+cases_cleaned = cases_cleaned.head(800000)
 #%%
-#cases_cleaned['date']= pd.to_datetime(cases_cleaned['date'])
 cases_cleaned['date'] = pd.to_datetime(cases_cleaned['date'], format='%Y/%m/%d')
-#cases_cleaned['date'] = cases_cleaned['date'].sort_values(ascending=True)
 cases_cleaned.sort_values(by='date', inplace=True)
 #cases_cleaned['date'] = cases_cleaned['date'].sort_values(ascending=True)
 cases_cleaned['date'] = cases_cleaned['date'].dt.strftime('%d/%m/%Y')
@@ -57,15 +55,31 @@ cases_cleaned_locations = cases_cleaned[["Confirmed","date","Lat", "Long_"]]
 cases_cleaned_locations_NA_removed = cases_cleaned_locations.fillna(8)
 #%%
 #cases_cleaned_locations_NA_removed['date']= pd.to_datetime(cases_cleaned_locations_NA_removed['date'])
-print(cases_cleaned.info())
+print(cases_cleaned_locations_NA_removed.info())
 
 # %%
-
+cases_cleaned_locations_NA_removed['date'] = pd.to_datetime(cases_cleaned_locations_NA_removed['date'])
+cases_cleaned_locations_NA_removed.sort_values(by='Confirmed', inplace=True)
 heat_info = []
+
+'''
+for date in cases_cleaned_locations_NA_removed['date'].sort_values().unique():
+    heat_info.append(cases_cleaned_locations_NA_removed.loc[cases_cleaned_locations_NA_removed['date'] == date, # for each hour append to list  
+    ['Lat', 'Long_', 'Confirmed']]\
+    .groupby(['Lat', 'Long_'])\
+    .sum().reset_index().values.tolist()) #sum totals per station, reset index and create list
+'''
+     
 for _, d in cases_cleaned_locations_NA_removed.groupby('date'):
    heat_info.append([[row['Lat'], row['Long_'], row['Confirmed']] for _, row in d.iterrows()])
+
+
+#for _, d in cases_cleaned_locations_NA_removed.groupby('Confirmed'):
+ #  heat_info.append([[row['Lat'], row['Long_']] for _, row in d.iterrows()])
    
+
 #%%
+#heat_info = cases_cleaned_locations_NA_removed[['Lat','Long_','Confirmed']].values.tolist()
 print(heat_info)
 # %%
 
@@ -89,8 +103,6 @@ hm = plugins.HeatMapWithTime(heat_info, auto_play=True,max_opacity=0.8)
 hm.add_to(covid_country_map)
 
 covid_country_map.save("mymap.html") 
-
-
 # %%
 '''
  #Centering the map with the latitude and longitude mean values 
@@ -121,3 +133,61 @@ folium.LayerControl().add_to(covid_country_map)
 
 # %%
 covid_country_map.save("mymap.html")   
+
+# %%
+cases_cleaned_Copy = pd.read_csv(file)
+#cases_cleaned_Copy = cases_cleaned_Copy.head(100000)
+# %%
+
+# %%
+group_CSSE = cases_cleaned_Copy[['Province_State','Deaths']].groupby('Province_State').sum().reset_index().sort_values("Deaths",ascending=False)
+#group_CSSE.head(5).plot(kind='bar',x='Province_State',y='Deaths',color='red',title = 'States with more deaths',rot=0)
+sns.barplot(x='Province_State', y='Deaths', data = group_CSSE.head(5)).set_title('States with more deaths')
+plt.show()
+print(cases_cleaned_Copy.head(5))
+#print(group_CSSE.sort_values("Deaths",ascending=False))
+# %%
+group_CSSE = cases_cleaned_Copy[['Province_State','Confirmed']].groupby('Province_State').sum().reset_index().sort_values("Confirmed",ascending=False)
+#group_CSSE.head(5).plot(kind='bar',x='Province_State',y='Confirmed',color='red',title = 'States with more confirmed',rot=0)
+sns.barplot(x='Province_State', y='Confirmed', data = group_CSSE.head(5)).set_title('States with more confirmed')
+plt.show()
+print(cases_cleaned_Copy.head(5))
+
+# %%
+group_CSSE = cases_cleaned_Copy[['Province_State','Recovered']].groupby('Province_State').sum().reset_index().sort_values("Recovered",ascending=False)
+#group_CSSE.head(5).plot(kind='bar',x='Province_State',y='Recovered',color='red',title = 'States with more Recovered',rot=0)
+sns.barplot(x='Province_State', y='Recovered', data = group_CSSE.head(5)).set_title('States with more recovered')
+plt.show()
+print(cases_cleaned_Copy.head(5))
+
+# %%
+group_CSSE = cases_cleaned_Copy[['Country_Region','Deaths']].groupby('Country_Region').sum().reset_index().sort_values("Deaths",ascending=False)
+#group_CSSE.head(5).plot(kind='bar',x='Country_Region',y='Deaths',color='red',title = 'Regions with more deaths',rot=0)
+sns.barplot(x='Country_Region', y='Deaths', data = group_CSSE.head(5)).set_title('Regions with more deaths')
+plt.show()
+print(cases_cleaned_Copy.head(5))
+
+# %%
+print(cases_cleaned_Copy.info())
+# %%
+cases_cleaned_Copy['date'] = pd.to_datetime(cases_cleaned_Copy['date'], format='%Y/%m/%d')
+cases_cleaned_Copy.sort_values(by='date', inplace=True)
+group_CSSE = cases_cleaned_Copy[['date','Confirmed']].groupby('date').sum().reset_index()
+
+group_CSSE['month'] = group_CSSE['date'].dt.month
+group_CSSE['year'] = group_CSSE['date'].dt.year
+group_CSSE['month'] = group_CSSE['month'].apply(lambda x: calendar.month_abbr[x])
+# %%
+group_CSSE = group_CSSE[['month','Confirmed','year']].groupby(['month','year']).sum().reset_index()
+
+# %%
+group_CSSE = group_CSSE.pivot(index='month', columns='year', values='Confirmed')
+
+#%%
+sns.heatmap(group_CSSE)
+
+# %%
+sns.heatmap(group_CSSE, fmt="f", annot=True, cmap='YlGnBu')
+# %%
+cases_cleaned_Copy.pivot(index='date', columns='Province_State', values='Confirmed')
+
