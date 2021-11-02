38d0f8de58410b739a84ff0372a27439bb07f93e
jisanos <44531911+jisanos@users.noreply.github.com>
2021-11-02 11:50:26 -0400

    started to fill nan values with interpolation

--- vaccines_analysis.py
+++ vaccines_analysis.py
@@ -31,16 +31,32 @@ vaccine_data_us_timeline = pd.read_csv(
 
 vaccine_data_us_timeline.drop('Combined_Key',axis=1,inplace=True)
 
+# %Dropping full nan rows
+
+vaccine_data_us_timeline.dropna(how='all', inplace=True)
+
+time_series_covid19_vaccine_global.dropna(how='all', inplace=True)
+
+
+vaccine_data_us_timeline.reset_index(inplace=True, drop = True)
+time_series_covid19_vaccine_global.reset_index(inplace=True, drop = True)
 # %%
-# Changing the date type to a datetime object
+# Assigning data types
 vaccine_data_us_timeline['Date'] = vaccine_data_us_timeline[
     'Date'].astype('datetime64[ns]')
+
+# vaccine_data_us_timeline = vaccine_data_us_timeline.convert_dtypes()
+
+time_series_covid19_vaccine_global['Date'] = time_series_covid19_vaccine_global['Date'].astype('datetime64[ns]')
+
+# time_series_covid19_vaccine_global = time_series_covid19_vaccine_global.convert_dtypes()
+
 
 # %%
 
 #Checking unique values of states in US dataset
 
-unique_us_states = set(vaccine_data_us_timeline['Province_State'])
+#unique_us_states = set(vaccine_data_us_timeline['Province_State'])
 
 # There are some values in the state column which arent actual US states
 # or are just departments related to the US states.
@@ -48,7 +64,7 @@ unique_us_states = set(vaccine_data_us_timeline['Province_State'])
 # %%
 
 # Creating a list of "states" in the vaccine_data_us_timeline which are departments
-from the US dataframe:
+# from the US dataframe:
 not_really_states = ["Long Term Care (LTC) Program",
                       "Veterans Health Administration",
                       "Bureau of Prisons", "Department of Defense",
@@ -63,7 +79,7 @@ not_really_states = ["Long Term Care (LTC) Program",
 
 # Checking unique value of country regions from US dataset
 
-unique_us_regions = set(vaccine_data_us_timeline['Country_Region'])
+#unique_us_regions = set(vaccine_data_us_timeline['Country_Region'])
 
 # We can see that US is the only value here
 
@@ -71,25 +87,23 @@ unique_us_regions = set(vaccine_data_us_timeline['Country_Region'])
 
 # Checking for unique values of Vaccine_Type from US dataset
 
-unique_vax_us = set(vaccine_data_us_timeline['Vaccine_Type'])
+#unique_vax_us = set(vaccine_data_us_timeline['Vaccine_Type'])
 
 # We can see that there are Unnassigned and Unknown values as well as Pfizer,
 # Moderna and Jannsen. Theres also an All which i assume contains a sum of them.
 
 # I believe Unknown and Unassigned can be merged as one
-# %%
 
-print(vaccine_data_us_timeline['Doses_admin'].max())
 # %%
 
 # Merging all rows that are unnassigned and unknown to a single one
 
-unknown_and_unnassigned = vaccine_data_us_timeline[
-    (vaccine_data_us_timeline["Vaccine_Type"] == 'Unassigned') | 
-    (vaccine_data_us_timeline["Vaccine_Type"] == 'Unknown')]
+# unknown_and_unnassigned = vaccine_data_us_timeline[
+#     (vaccine_data_us_timeline["Vaccine_Type"] == 'Unassigned') | 
+#     (vaccine_data_us_timeline["Vaccine_Type"] == 'Unknown')]
 
-# Dropping the unknown and unnassigned from the main dataframe
-vaccine_data_us_timeline.drop(index = unknown_and_unnassigned.index, inplace = True)
+# # Dropping the unknown and unnassigned from the main dataframe
+# vaccine_data_us_timeline.drop(index = unknown_and_unnassigned.index, inplace = True)
 
 # Doing a common sum of values of Unknown and unnassigned on equal 
 # country, state, and date
@@ -98,16 +112,17 @@ vaccine_data_us_timeline.drop(index = unknown_and_unnassigned.index, inplace = T
 #                                  'Country_Region','FIPS','Lat','Long_',
 #                                  ])
 
+#Filtering in all with Unnasigned value
+filter_in = (vaccine_data_us_timeline["Vaccine_Type"].str.lower().str.contains('unassigned'))
 
+# Reassigning the filtered ones to Unknown
+vaccine_data_us_timeline.loc[filter_in, 'Vaccine_Type'] = 'Unknown'
+# This will possibly create duplicate entries that we can handle by merging them
+# with a sum
 
-# del unknown_and_unnassigned # Deleting from memory
+dups = vaccine_data_us_timeline[vaccine_data_us_timeline.duplicated(['Date','Province_State','Country_Region','Vaccine_Type'])]
 
-
-# %%
-# Changing date column to a date variable
-
-time_series_covid19_vaccine_global['Date'] = time_series_covid19_vaccine_global['Date'].astype('datetime64[ns]')
-
+# There dont seem to be any duplicates after the renaming.
 
 # %%
 
@@ -126,7 +141,7 @@ unique_global_regions = set(time_series_covid19_vaccine_global["Country_Region"]
 unique_global_states = set(time_series_covid19_vaccine_global['Province_State'])
 
 unique_global_regions_with_nan_states = set(
-    time_series_covid19_vaccine_global[~time_series_covid19_vaccine_global['Province_State'].notna()]['Country_Region'])
+    time_series_covid19_vaccine_global[time_series_covid19_vaccine_global['Province_State'].isna()]['Country_Region'])
 
 # %%
 
@@ -142,9 +157,10 @@ unique_global_regions_with_nan_states = set(
 to_remove = ["US", "US (Aggregate)", "World"]
 
 for element in to_remove:
-
-    time_series_covid19_vaccine_global = time_series_covid19_vaccine_global[~(
-        time_series_covid19_vaccine_global["Country_Region"] == element)]
+    
+    filter_out = np.logical_not(time_series_covid19_vaccine_global["Country_Region"] == element)
+    
+    time_series_covid19_vaccine_global = time_series_covid19_vaccine_global[filter_out]
 
 
 # %%
@@ -171,13 +187,13 @@ for element in to_remove:
 unique_us_states = set(vaccine_data_us_timeline['Province_State'])
 unique_global_states = set(time_series_covid19_vaccine_global['Province_State'])
 
-in_both = [
-    element for element in unique_us_states if element in unique_global_states]
+in_both = unique_us_states.intersection(unique_global_states)
 
 # According to this, they both have unique values of state by this point
+# This means the global dataset only contains US values on a country level
 # %%
 # Adding Vaccine_Type column equaling to all for the global dataset for the merging
-# time_series_covid19_vaccine_global["Vaccine_Type"] = "All"
+time_series_covid19_vaccine_global["Vaccine_Type"] = "All"
 
 # %%
 # The column "People_partially_vaccinated" from the global dataset is the most
@@ -185,28 +201,126 @@ in_both = [
 # This goes the same for the People_fully_vaccinated and Stage_two_doses
 # so these will be renamed for the merger
 
-# time_series_covid19_vaccine_global.rename(columns={"People_partially_vaccinated": "Stage_One_Doses",
-#                               "People_fully_vaccinated": "Stage_Two_Doses"}, inplace=True)
+time_series_covid19_vaccine_global.rename(columns={"People_partially_vaccinated": "Stage_One_Doses",
+                              "People_fully_vaccinated": "Stage_Two_Doses"}, inplace=True)
 
 # These columns seem to contain lots of nans at times so it might not be as
 # important as Doses_admin
 
 # %%
 # Merging global dataframe with us dataframe
-# merge_df = pd.merge(vaccine_data_us_timeline, time_series_covid19_vaccine_global, how='outer',
-#                     on=['Country_Region', 'Province_State', 'Vaccine_Type', 'Doses_admin',
-#                         'Stage_One_Doses', 'Stage_Two_Doses', 'Date'])
+vax_df = pd.merge(vaccine_data_us_timeline, time_series_covid19_vaccine_global, how='outer',
+                    on=['Country_Region', 'Province_State', 'Vaccine_Type', 'Doses_admin',
+                        'Stage_One_Doses', 'Stage_Two_Doses', 'Date'])
+
+# %% Dropping some unnecesary columns
+
+drop = ['Lat', 'Long_','Report_Date_String']
+
+vax_df = vax_df.drop(drop, axis=1)
+
+# %% Resetting index
+
+vax_df.reset_index(inplace=True, drop = True)
+
+#%% Assigning propper datatypes
+
+# vax_df = vax_df.convert_dtypes()
+# %%some dtypes are not being assigned propperly.
+# Assigning float instead of int for interpolation to work
+
+dtypes = {'Doses_admin': "float64",
+          'Stage_One_Doses': "float64",
+          'Stage_Two_Doses': "float64",
+          'Doses_alloc':'float64',
+          'Doses_shipped':'float64'}
+
+vax_df = vax_df.astype(dtypes)
+
+# %% Make sure all numbers are positive
+abs_cols = ['Doses_alloc', 'Doses_shipped', 'Doses_admin', 'Stage_One_Doses',
+            'Stage_Two_Doses']
+
+for col in abs_cols:
+    vax_df[col] = vax_df[col].abs()
+
+# %%
+# Stripping string columns
+
+str_cols = ['Province_State', 'Vaccine_Type', 'Country_Region']
+
+for col in str_cols:
+    vax_df[col] = vax_df[col].str.strip()
 
 # %%
 
 #Df with only "All" vaccine types
 
 
-# total_global_vax = merge_df[["Province_State", "Vaccine_Type", "Country_Region",
+# total_global_vax = vax_df[["Province_State", "Vaccine_Type", "Country_Region",
 #                              "Doses_admin", "Stage_One_Doses",
 #                              "Stage_Two_Doses"
 #                              ]].groupby(["Province_State","Country_Region","Vaccine_Type"]).sum().reset_index()
 
+#%%
+
+# vax_df.sort_values([''])
+
+vax_df.sort_values('Date').reset_index(inplace = True, drop = True)
 
 
+# %% Filling nan values, by interpolation
 
+def filler(x):
+    
+    
+    # Assumes data is already sorted by date
+    
+    # Simple forward filling
+    # x.Doses_alloc = x.Doses_alloc.ffill().bfill()
+    # x.Doses_shipped = x.Doses_shipped.ffill().bfill()
+    # x.Doses_admin = x.Doses_admin.ffill().bfill()
+    # x.Stage_One_Doses = x.Stage_One_Doses.ffill().bfill()
+    # x.Stage_Two_Doses = x.Stage_Two_Doses.ffill().bfill()
+    
+    #Setting first values to 0 for interpolation to work best
+    
+    first_index = x.head(1).index
+    
+    x.loc[first_index,'Doses_alloc'] = 0
+    x.loc[first_index,'Doses_shipped'] = 0
+    x.loc[first_index,'Doses_admin'] = 0
+    x.loc[first_index,'Stage_One_Doses'] = 0
+    x.loc[first_index,'Stage_Two_Doses'] = 0
+    
+    # Interpolation filling. Making sure they are all round numbers
+    x['Doses_alloc'] = x['Doses_alloc'].interpolate().round()
+    x['Doses_shipped'] = x['Doses_shipped'].interpolate().round()
+    x['Doses_admin'] = x['Doses_admin'].interpolate().round()
+    x['Stage_One_Doses'] = x['Stage_One_Doses'].interpolate().round()
+    x['Stage_Two_Doses'] = x['Stage_Two_Doses'].interpolate().round()
+    
+    
+    return x
+
+vax_df = vax_df.groupby(['Province_State','Country_Region','Vaccine_Type'],dropna=False).apply(filler)
+
+# %% Creating column with new vaccines only, instead of cummulative
+
+def date_cases(x):
+        
+    
+    # x['New_Doses_admin'] = x.Doses_admin.sub(x.Doses_admin.shift().fillna(0)).abs()
+    
+    # x['Doses_admin'] = x['New_Doses_admin'].cumsum()
+    
+    return x
+    
+
+new_df = vax_df = vax_df.groupby(['Province_State','Country_Region','Vaccine_Type'],dropna=False).apply(date_cases)
+
+
+# %%
+
+test_df = vax_df[(new_df['Province_State'] == 'American Samoa') &
+                 (new_df['Vaccine_Type'] == 'All')]
