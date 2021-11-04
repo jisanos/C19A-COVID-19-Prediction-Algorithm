4192c60cbea6f3e53576fa73074bb037e8f50e28
Seals <59105007+RaynielRamirezz@users.noreply.github.com>
2021-11-03 20:26:39 -0400

    Big changes for optimitazion and understanding the visualization of the data

--- cases_cleaned_categorizable_visualization.py
+++ cases_cleaned_categorizable_visualization.py
@@ -31,11 +31,12 @@ ProvinceUsesOnly = cases_cleaned_categorizable[cases_cleaned_categorizable['Coun
               cases_cleaned_categorizable['Province_State'].notna() &
               cases_cleaned_categorizable['Admin2'].isna()]
 
+ProvinceUsesOnly_Correlation =  ProvinceUsesOnly[ProvinceUsesOnly['Deaths'] < 100000]
 # %%
 # group by province state but in order of the highest death cases
 #group_CSSE = ProvinceUsesOnly.sort_values(['New_Deaths','Province_State','Country_Region'],ascending=False).reset_index()
-group_CSSE = ProvinceUsesOnly[['Province_State','Deaths']].sort_values('Deaths',ascending=False)
-# %%
+group_CSSE = ProvinceUsesOnly_Correlation[['Province_State','Deaths','Confirmed']]
+
 # making a joint plot of provincestate
 sns.jointplot(data=group_CSSE, x="Confirmed", y="Deaths", kind="reg")
 plt.suptitle("Province state regarding the correlation of confirmed cases and death cases")
@@ -60,16 +61,18 @@ ProvinceUsesOnly_without_date_filter = cases_cleaned_categorizable_Without_Date_
     & cases_cleaned_categorizable_Without_Date_Filter['Province_State'].notna() &
               cases_cleaned_categorizable_Without_Date_Filter['Admin2'].isna()]
 
-ProvinceUsesOnly_without_date_filter =  ProvinceUsesOnly_without_date_filter[ProvinceUsesOnly_without_date_filter['New_Deaths'] < 30]
+ProvinceUsesOnly_without_date_filter =  ProvinceUsesOnly_without_date_filter[
+    ProvinceUsesOnly_without_date_filter['New_Deaths'] < 30]
 # %%
+'''
 # group by province state but in order of the highest death cases
-#group_CSSE = ProvinceUsesOnly.sort_values(['New_Deaths','Province_State','Country_Region'],ascending=False).reset_index()
-group_CSSE = ProvinceUsesOnly_without_date_filter[['Province_State','New_Deaths']].sort_values('New_Deaths',ascending=False)
+group_CSSE = ProvinceUsesOnly_without_date_filter[['Province_State','New_Deaths']]
 #plot a violin plot to see the confirmed and deaths of each province state as weight
 #but with the (top 5 regarding the confirmed cases and deaths)
-top5 = ProvinceUsesOnly_without_date_filter[['Province_State','New_Deaths']].groupby('Province_State').max().reset_index().sort_values('New_Deaths',ascending=False)
+top5 = ProvinceUsesOnly_without_date_filter[['Province_State','New_Deaths']].groupby(
+    'Province_State').max().reset_index().sort_values('New_Deaths',ascending=False)
 filteringViolin = group_CSSE[group_CSSE['Province_State'].isin(set(top5['Province_State'].head(4)))]
-sns.violinplot(x='Province_State', y='New_Deaths', data = filteringViolin)
+sns.violinplot(x='Province_State', y='New_Deaths', data = filteringViolin) '''
 # %%
 
 # %%
@@ -93,14 +96,22 @@ group_CSSE = CountryUsesOnly[['Country_Region','Deaths']].sort_values("Deaths",a
 
 #plot a pie chart regarding the top 5 deaths of countries
 group_CSSE = group_CSSE.set_index('Country_Region')
-group_CSSE.head(5).plot.pie(y='Deaths', figsize=(9, 9),autopct='%1.1f%%')
+group_CSSE.head(10).plot.pie(y='Deaths', figsize=(12, 9),autopct='%1.1f%%')
 
 # %%
 # group by country region
-group_CSSE = CountryUsesOnly[['Country_Region','Deaths']].sort_values("Deaths",ascending=False)
+group_CSSE = CountryUsesOnly[['Country_Region','Confirmed']].sort_values("Confirmed",ascending=False)
+
+#plot a pie chart regarding the top 5 deaths of countries
+group_CSSE = group_CSSE.set_index('Country_Region')
+group_CSSE.head(10).plot.pie(y='Confirmed', figsize=(10, 10),autopct='%1.1f%%')
+
+# %%
+# group by country region
+group_CSSE = CountryUsesOnly[['Country_Region','Confirmed']].sort_values("Confirmed",ascending=False)
 
 #a bar plot to show the top 5 deaths of countries
-sns.barplot(x='Country_Region', y='Deaths', data = group_CSSE.head(5)).set_title('Regions with more deaths')
+sns.barplot(x='Country_Region', y='Confirmed', data = group_CSSE.head(5)).set_title('Regions with more deaths')
 plt.show()
 
 # %%
@@ -111,7 +122,24 @@ ProvinceUsesOnlyForUs = ProvinceUsesOnly[ProvinceUsesOnly['Country_Region'] == '
 #US states most deaths only
 group_CSSE = ProvinceUsesOnlyForUs[['Province_State','Deaths']].sort_values('Deaths',ascending=False)
 #bar plot regarding the top 5 total confirmed cases when it comes to province state
-sns.barplot(x='Province_State', y='Deaths', data = group_CSSE.head(5)).set_title('States with more confirmed')
+sns.barplot(x='Province_State', y='Deaths', data = group_CSSE.head(5)).set_title('US States with more deaths')
 plt.show()
 
+# %%
+#Filtering cases cleaned categorizable for only province state uses
+CountryUsesOnly_without_date_filter = cases_cleaned_categorizable_Without_Date_Filter[
+    cases_cleaned_categorizable_Without_Date_Filter['Country_Region'].notna() 
+    & cases_cleaned_categorizable_Without_Date_Filter['Province_State'].isna() &
+              cases_cleaned_categorizable_Without_Date_Filter['Admin2'].isna()]
 
+CountryUsesOnly_without_date_filter =  CountryUsesOnly_without_date_filter[
+    CountryUsesOnly_without_date_filter['New_Deaths'] < 1000]
+# %%
+# group by province state but in order of the highest death cases
+group_CSSE = CountryUsesOnly_without_date_filter[['Country_Region','New_Deaths']]
+#plot a violin plot to see the confirmed and deaths of each province state as weight
+#but with the (top 5 regarding the confirmed cases and deaths)
+top5 = CountryUsesOnly_without_date_filter[['Country_Region','Deaths']].groupby(
+    'Country_Region').max().reset_index().sort_values('Deaths',ascending=False)
+filteringViolin = group_CSSE[group_CSSE['Country_Region'].isin(set(top5['Country_Region'].head(4)))]
+sns.violinplot(x='Country_Region', y='New_Deaths', data = filteringViolin)
